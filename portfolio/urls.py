a3c16f162ecec4f208a756f36048c72ec8f597c6
isaiah olaoye <olaisaiah54@gmail.com>
2021-05-06 14:12:30 +0100

    Added django-environ

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 from django.urls import path, include
@@ -26,4 +27,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
